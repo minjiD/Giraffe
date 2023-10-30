1537bc9468a77434eae130ed6da42df81a7aac7b
minji <ssosso12347@gmail.com>
2023-10-30 18:40:51 +0900

    변수 선언 및 출력 방식

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string charecterName = "Tom";
+            int charecterAge = 25;
+
+            Console.WriteLine("There once was a man named " + charecterName);
+            Console.WriteLine("He was " + charecterAge + " years old");
+
+            charecterName = "Mike";
+            Console.WriteLine("He really liked the name " + charecterName);
+            Console.WriteLine("But didn't like being " + charecterAge);
+
+
+            Console.ReadLine();
         }
     }
 }
