f63153436f8a2a7ec499bedae0ab837c84baff65
minji <ssosso12347@gmail.com>
2023-10-30 19:15:41 +0900

    연산자 연산 내장 함수 - Abs, Pow, Sqrt, Max, Min, Round

--- Program.cs
+++ Program.cs
@@ -4,20 +4,27 @@
     {
         static void Main(string[] args)
         {
-            string phrase = "Giraffe Academy";
-            char grade = 'A';
-            int age = -30;
-            double gpa = 3.2; //float, double, decimal
-            bool isFemale = true;
 
-            Console.WriteLine(phrase.Length);
-            Console.WriteLine(phrase.ToUpper());
-            Console.WriteLine(phrase.ToLower());
-            Console.WriteLine(phrase.Contains("Academies"));
-            Console.WriteLine(phrase[0]);
-            Console.WriteLine(phrase.IndexOf("Academy")); //값이 없으면 -1 return
-            Console.WriteLine(phrase.Substring(8));
-            Console.WriteLine(phrase.Substring(8, 3));
+            Console.WriteLine(5 + 8); //13
+            Console.WriteLine(5 - 8); //-3
+            Console.WriteLine(5 * 8); //40
+            Console.WriteLine(5 / 8); //0
+            Console.WriteLine(5 % 2); //1
+            Console.WriteLine((4 + 2) * 3); //18
+            Console.WriteLine(5.0 + 8.1); //13.1
+            Console.WriteLine(5 / 2.0); //2.5
+
+            int num = 6;
+            num++; //7
+            //num--;
+            Console.WriteLine(num);
+
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3.8, 2));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(4, 90));
+            Console.WriteLine(Math.Min(4, 90));
+            Console.WriteLine(Math.Round(4.6));
 
             Console.ReadLine();
         }
