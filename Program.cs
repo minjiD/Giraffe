cba25d4f5e9a311ae75e07eb17e60fa49053d2eb
minji <ssosso12347@gmail.com>
2023-10-30 19:02:58 +0900

    내장 함수 - Length, ToUpper, ToLower, Contains, IndexOf, Substring

--- Program.cs
+++ Program.cs
@@ -4,13 +4,20 @@
     {
         static void Main(string[] args)
         {
-            string phrase = "Giraffe Accademy";
+            string phrase = "Giraffe Academy";
             char grade = 'A';
             int age = -30;
             double gpa = 3.2; //float, double, decimal
             bool isFemale = true;
 
-            Console.WriteLine(age);
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.Contains("Academies"));
+            Console.WriteLine(phrase[0]);
+            Console.WriteLine(phrase.IndexOf("Academy")); //값이 없으면 -1 return
+            Console.WriteLine(phrase.Substring(8));
+            Console.WriteLine(phrase.Substring(8, 3));
 
             Console.ReadLine();
         }
