b8e9343e303aede817faf5cfca9990a4a58ed386
minji <ssosso12347@gmail.com>
2023-10-31 17:56:49 +0900

    문자열 -> 숫자 변환

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@
     {
         static void Main(string[] args)
         {
+            int num = Convert.ToInt32("45"); //문자 X
+            Console.WriteLine(num + 6);
 
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " you are " + age);
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine(num1 + num2);
             
             Console.ReadLine();
         }
