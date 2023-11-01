2833415105a3d6e2dde5596a0355570562cbf1c2
minji <ssosso12347@gmail.com>
2023-11-01 19:00:59 +0900

    While Loops

--- Program.cs
+++ Program.cs
@@ -4,45 +4,23 @@
     {
         static void Main(string[] args)
         {
-            //Switch Statements
-            Console.WriteLine(GetDay(0));
-
-            Console.ReadLine();
-        }
-
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch(dayNum)
+            //While Loops
+            int index = 1;
+            while (index <= 5)
             {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invaild Day Number";
-                    break;
+                Console.WriteLine(index);
+                index++;
             }
 
-            return dayName;
+            //Do While Loops
+            int idx = 6;
+            do
+            {
+                Console.WriteLine(idx);
+                idx++;
+            } while(idx <= 5);
+
+            Console.ReadLine();
         }
 
     }
