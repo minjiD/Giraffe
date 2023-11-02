842902285076ed4b38a37a182b034df4c2e48697
minji <ssosso12347@gmail.com>
2023-11-02 16:40:00 +0900

    Exception Handling

--- Program.cs
+++ Program.cs
@@ -4,13 +4,27 @@
     {
         static void Main(string[] args)
         {
-            /* Comments - multiple lines
-             * 
-             * 
-             */
-            
-            //Comments
-            Console.WriteLine("Comments");
+            //Exception Handling
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e) { 
+                Console.WriteLine(e.Message);
+            }
+            finally 
+            {
+                Console.WriteLine();
+            }
 
             Console.ReadLine();
         }
