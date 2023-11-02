553827f2d9fb486fb097842231883d806ac2536a
minji <ssosso12347@gmail.com>
2023-11-02 17:24:42 +0900

    Classes & Objects

--- Program.cs
+++ Program.cs
@@ -4,27 +4,21 @@
     {
         static void Main(string[] args)
         {
-            //Exception Handling
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
+            //Classes & Objects
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
 
-                Console.WriteLine(num1 / num2);
-            }
-            catch(DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch(FormatException e) { 
-                Console.WriteLine(e.Message);
-            }
-            finally 
-            {
-                Console.WriteLine();
-            }
+            Book book2 = new Book();
+            book2.title = "Lord Of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.pages);
+
+            Console.WriteLine(book2.author);
 
             Console.ReadLine();
         }
